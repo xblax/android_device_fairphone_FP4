a8c872c5de95258fc74f5ea4290ebe28d06f14cb
xblax <xblax@incy.net>
2022-04-10 22:50:27 +0200

    FP4: install stock camera app instead of Snap

--- device.mk
+++ device.mk
@@ -191,6 +191,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
+# Stock Camera App
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-com.fp.camera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.fp.camera.xml
+
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
