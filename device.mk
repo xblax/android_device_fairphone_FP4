a1b0f71b357457e3dc7811011e8fbc89dff15e52
xblax <xblax@incy.net>
2022-08-14 14:00:49 +0200

    FP4: device.mk: nfc: build com.nxp.nfc.nq
    Align with stock rom.

--- device.mk
+++ device.mk
@@ -311,6 +311,7 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2.vendor:64 \
     android.hardware.secure_element@1.2.vendor:64 \
     com.android.nfc_extras \
+    com.nxp.nfc.nq \
     NfcNci \
     SecureElement \
     Tag
